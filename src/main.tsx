a4f84315e49aa224609be2ff5cc6a28b27734c0f
xb4rn3y <willbarnes955@gmail.com>
2025-10-01 16:24:01 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './components/ThemeProvider'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="light" storageKey="albury-theme">
+    <App />
+  </ThemeProvider>
+);
